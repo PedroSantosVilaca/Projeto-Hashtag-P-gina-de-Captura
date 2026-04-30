0db47e67fbd59d88e41db1779d80abf2a9d5c8c6
Pedro Vilaça <eu.sou.pedro.vilaca@gmail.com>
2026-04-30 17:38:12 -0300

    popup quase pronto

--- pagina-captura/src/main.js
+++ pagina-captura/src/main.js
@@ -4,8 +4,11 @@ document.addEventListener("DOMContentLoaded", () => {
   const botaoTopo = document.getElementById("botao-conteiner1");
   const primeiraDobra = document.getElementById("primeira-dobra");
   botaoTopo.addEventListener("click", () => {
-    primeiraDobra.innerHTML += `<div id="popup-header"  class ="popup-header"><div class="popup-dentro">
-    <h1 class="texto-popup">Fale conosco!</h1><input type= "text" placeholder= "digite seu nome" id= "nome"></div>
+    primeiraDobra.innerHTML += `<div id="popup-header"  class ="popup-header"><div id= "botao-fechar"><p id= "fechar">x</p></div><div class="popup-dentro">
+    <input type= "text" placeholder= "    *digite seu nome" id= "nome">
+    <input type= "text" placeholder= "    *digite seu melhor email" id= "email">
+    <input type= "number" placeholder= "    *digite seu DDD + Telefone" id= "telefone">
+    <button id= "botao-formulario">QUERO GARANTIR MINHA VAGA!</button></div>
     </div>`;
 
     const popUpHead = document.getElementById("popup-header");
