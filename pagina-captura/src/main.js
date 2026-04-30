21cef12e27b9f89c3bd6c2f307c7887daa0a0824
Pedro Vilaça <eu.sou.pedro.vilaca@gmail.com>
2026-04-30 04:45:34 -0300

    comecei o pop up

--- pagina-captura/src/main.js
+++ pagina-captura/src/main.js
@@ -1,1 +1,13 @@
 import "./style.css";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const botaoTopo = document.getElementById("botao-conteiner1");
+  const primeiraDobra = document.getElementById("primeira-dobra");
+  botaoTopo.addEventListener("click", () => {
+    primeiraDobra.innerHTML = `<div id="popup-header"  class ="popup-header"><div class="popup-dentro">
+    <h1 class="texto-popup">Fale conosco!</h1></div></div>`;
+
+    const popUpHead = document.getElementById("popup-header");
+    popUpHead.style.display = "flex";
+  });
+});
