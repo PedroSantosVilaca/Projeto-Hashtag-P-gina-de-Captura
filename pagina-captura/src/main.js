4d9ef8b6c94eb550dc28b30b685186182089b39b
Pedro Vilaça <eu.sou.pedro.vilaca@gmail.com>
2026-05-02 14:23:16 -0300

    popup funcionando

--- pagina-captura/src/main.js
+++ pagina-captura/src/main.js
@@ -4,14 +4,25 @@ document.addEventListener("DOMContentLoaded", () => {
   const botaoTopo = document.getElementById("botao-conteiner1");
   const primeiraDobra = document.getElementById("primeira-dobra");
   botaoTopo.addEventListener("click", () => {
-    primeiraDobra.innerHTML += `<div id="popup-header"  class ="popup-header"><div id= "botao-fechar"><p id= "fechar">x</p></div><div class="popup-dentro">
-    <input type= "text" placeholder= "    *digite seu nome" id= "nome">
-    <input type= "text" placeholder= "    *digite seu melhor email" id= "email">
-    <input type= "number" placeholder= "    *digite seu DDD + Telefone" id= "telefone">
-    <button id= "botao-formulario">QUERO GARANTIR MINHA VAGA!</button></div>
-    </div>`;
+    const botaoFechar = document.getElementById("botao-fechar");
+    const popUpHead = document.getElementById("popup-header-off");
 
-    const popUpHead = document.getElementById("popup-header");
-    popUpHead.style.display = "flex";
+    popUpHead.classList.add("popup-header");
+
+    botaoFechar.addEventListener("click", () => {
+      popUpHead.classList.remove("popup-header");
+    });
+  });
+
+  const nome = document.getElementById("nome");
+  const email = document.getElementById("email");
+  const telefone = document.getElementById("telefone");
+  const botaoFormulario = document.getElementById("botao-formulario");
+
+  botaoFormulario.addEventListener("click", () => {
+    console.log(nome.value, email.value, telefone.value);
+    nome.value = "";
+    telefone.value = "";
+    email.value = "";
   });
 });
